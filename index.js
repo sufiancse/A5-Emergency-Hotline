e540d2769883fa036afce7f70e9a99085f12b849
sufuancse <sufian.cse24@gmail.com>
2025-08-27 13:41:49 +0600

    design section compllete and functionalities started

---
+++ index.js
@@ -0,0 +1,11 @@
+function getElement(id){
+    const getId = document.getElementById(id);
+    return getId;
+}
+
+document.getElementById('cards-container').addEventListener('click', function(e){
+    if(e.target.className.includes('call-btn')){
+        
+        const
+    }
+})
